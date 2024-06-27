d6bb95e84f9b1353752119430e86cd40712f8958
ackermanjayjay <reza23198@gmail.com>
2024-06-27 17:15:29 +0700

    fix: <snake_case pep8>

--- load_model.py
+++ load_model.py
@@ -1,12 +1,15 @@
 import pickle
 import os
 import logging
+
+
 def prediction(feature):
-    try :
-        predictor_load = pickle.load(open(os.path.join("model", "model_tree.pkl"), "rb"))
+    try:
+        predictor_load = pickle.load(
+            open(os.path.join("model", "model_tree.pkl"), "rb")
+        )
         result = predictor_load.predict([feature])
-        logging.info('prediction load')
+        logging.info("prediction load")
         return result
     except Exception as e:
         logging.error(f"could not insert data due to {e}")
-
