c596b36f42e24541eb55022d8cd701f9c168a013
ackermanjayjay <reza23198@gmail.com>
2024-06-27 15:55:54 +0700

    fix <update preprocessing backend server to when user input process in model, server.py just readeblew>

--- model.py
+++ model.py
@@ -1,11 +1,38 @@
 from pydantic import BaseModel
+from load_model import prediction
+import numpy as np
+import logging
+
+from preprocess import loadInputIterative
 
 
 class InputData(BaseModel):
     StudyTimeWeekly: float
-    Absences: float
-    Tutoring: float
-    ParentalSupport: float
-    Sports: float
-    Music: float
-    Volunteering: float
+    Absences: int
+    Tutoring: int
+    ParentalSupport: int
+    Sports: int
+    Music: int
+    Volunteering: int
+
+
+def classification_student(data: InputData):
+    try:
+        input_array = np.array(
+            [
+                data.StudyTimeWeekly,
+                data.Absences,
+                data.Tutoring,
+                data.ParentalSupport,
+                data.Sports,
+                data.Music,
+                data.Volunteering,
+            ]
+        )
+        result = prediction(input_array)
+        return {
+            "prediction": result[0],
+            "Input": loadInputIterative(input_array),
+        }
+    except Exception as e:
+        logging.error("status_code = ", 500, str(e))
