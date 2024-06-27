b07cfd53b122d16dd46bc20cc8ed2e0d5f803293
ackermanjayjay <reza23198@gmail.com>
2024-06-27 15:56:36 +0700

    fix: <adding return to store to model when return in server json>

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,24 @@
 from pydantic import BaseModel
-def preprocess_feature_data (data):
-   tutoring = {
-      "Yes":1.0,
-      "No":0.0
-   }
-   ParentalSupport = {
-      
-   }
-   
+import logging
+
+
+def preprocess_feature_data(data):
+    tutoring = {"Yes": 1.0, "No": 0.0}
+    ParentalSupport = {}
+
+
 def loadInputIterative(data):
-   simpan = []
-   for value in data:
-      simpan.append(value)
-   return simpan
- 
+    simpan = []
+    result = {}
+    for value in data:
+        simpan.append(value)
+    result = {
+        "StudyTimeWeekly": simpan[0],
+        "Absences": simpan[1],
+        "ParentalSupport": simpan[2],
+        "Tutoring": simpan[3],
+        "Sports": simpan[4],
+        "Music": simpan[5],
+        "Volunteering": simpan[6],
+    }
+    return result
