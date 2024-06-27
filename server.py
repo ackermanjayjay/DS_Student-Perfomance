90864e21e9804ca9b74ce76304c1398113fcbe3e
ackermanjayjay <reza23198@gmail.com>
2024-06-27 15:22:53 +0700

    fix: beauty model base from input user

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ from pydantic import BaseModel
 from model import InputData
 import numpy as np
 
+from preprocess import loadInputIterative
+
 app = FastAPI()
 
 
@@ -16,20 +18,25 @@ def read_root():
 
 
 @app.post("/prediction/")
+
 def classification(data: InputData):
     try:
         input_array = np.array(
             [
-                data.feature1,
-                data.feature2,
-                data.feature3,
-                data.feature4,
-                data.feature5,
-                data.feature6,
-                data.feature7,
+                data.StudyTimeWeekly,
+                data.Absences,
+                data.Tutoring,
+                data.ParentalSupport,
+                data.Sports,
+                data.Music,
+                data.Volunteering,
             ]
         )
         result = prediction(input_array)
-        return {"Statuscode": 200, "prediction": result[0]}
+        return {
+            "Statuscode": 200,
+            "prediction": result[0],
+            "Input": loadInputIterative(input_array),
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
